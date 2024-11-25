2be6bb86475bcf1862dbea377453aa5a8edfcaf1
Alexandre-Savariradjane <alexandre.savariradjane@edu.esiee.fr>
2024-11-25 15:09:57 +0100

    Update player.py

--- player.py
+++ player.py
@@ -1,5 +1,33 @@
 # Define the Player class.
 class Player():
+    """
+    This class represents a Player. A room is composed of a name, and a current location represented by the room where he is.
+
+    Attributes:
+        name (str): The name.
+        current_room (bool) : The room where the player is currently located.
+
+    Methods:
+        __init__(self, name, description) : The constructor.
+        move(self, direction): Moves the player to a room in the specified direction if possible.
+
+    Examples:
+
+    >>> from room import Room
+    >>> room1 = Room("Forest", "dans une forêt enchantée. Vous entendez une brise légère à travers la cime des arbres.")
+    >>> room2 = Room("Cave", "dans une grotte profonde et sombre. Des voix semblent provenir des profondeurs.")
+    >>> room1.exits["N"] = room2
+    >>> player = Player("Nom_de_la_personne")
+    >>> player.current_room = room1
+    >>> player.move("N")
+    '\nVous êtes une cave.\n\nSorties: 'S'
+    >>> player.current_room.name
+    'Forest'
+    >>> player.move("S")
+    '\nAucune porte dans cette direction !\n'
+    False
+    
+    """
 
     # Define the constructor.
     def __init__(self, name):
