db41c5db323867d2b127de8a40817527ec0ab408
Alexandre-Savariradjane <alexandre.savariradjane@edu.esiee.fr>
2024-11-25 15:10:34 +0100

    Update room.py

--- room.py
+++ room.py
@@ -1,6 +1,39 @@
 # Define the Room class.
 
 class Room:
+    """
+    This class represents a room. A room is composed of a name, an exit and a description.
+
+    Attributes:
+        name (str): The name.
+        description (str) : A quick description of the room.
+        exit (dict): A dictionary mapping directions (str) to adjacent Room instances
+
+
+    Methods:
+        __init__(self, name, description) : The constructor.
+        get_exit(self, direction): Returns the room connected in the given direction if it exists, otherwise None.
+        get_exit_string(self): Returns a formatted string describing the available exits.
+        get_long_description(self): Returns a long description of the room, including its exits.
+
+
+    Examples:
+
+    >>> room = Room("Forest", "dans une forêt enchantée. Vous entendez une brise légère à travers la cime des arbres.")
+    >>> room.name
+    'Forest'
+    >>> room.description
+    'dans une forêt enchantée. Vous entendez une brise légère à travers la cime des arbres.'
+    >>> room.exits
+    {}
+    >>> room.exits["N"] = Room("Cave", "dans une grotte profonde et sombre. Des voix semblent provenir des profondeurs.")
+    >>> room.get_exit("N").name
+    'Cave'
+    >>> room.get_exit_string()
+    'Sorties: N'
+    >>> print(room.get_long_description())
+    '\nVous dans une forêt enchantée. Vous entendez une brise légère à travers la cime des arbres.\n\nSorties: N\n'
+    """
 
     # Define the constructor. 
     def __init__(self, name, description):
